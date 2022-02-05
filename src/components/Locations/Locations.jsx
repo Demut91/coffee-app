86ef3d2e660c9195615087b3c234c29e2228b939
root <root@DESKTOP-IONV53U.localdomain>
2022-02-05 11:59:06 +0800

    refactoring with axios

--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -1,46 +1,35 @@
-//import { useEffect } from "react";
-//import { v4 as uuidv4 } from "uuid";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 function Locations() {
-  let TOKEN =
-    "";
+  const [locations, setLocations] = useState(null);
 
-  function makeGet(url) {
-    let xhr = new XMLHttpRequest();
-    return new Promise(function (resolve, reject) {
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) resolve(xhr);;
-        }
-             else reject({
-                status: xhr.status,
-                statusText: xhr.statusText
-            });
-      };
+  useEffect(() => {
+    let TOKEN =
+      "";
 
-      xhr.open("GET", url, false);
-      xhr.setRequestHeader("Authorization", "Bearer " + TOKEN);
-      // xhr.setRequestHeader("Accept", "application/json");
-      // xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send();
-    });
-  }
+    axios.defaults.headers.common["Authorization"] = "Bearer " + TOKEN;
+    axios
+      .get("https://cors-anywhere.herokuapp.com/185.244.172.108:8080/locations")
+      .then((response) => {
+        setLocations(response.data);
+      });
+  }, [setLocations]);
 
-  makeGet(
-    "https://cors-anywhere.herokuapp.com/185.244.172.108:8080/locations"
-  );
+  console.log(locations);
 
   return (
     <div>
       <h1>Locations</h1>
 
-      {/* {locations.map((location) => (
+      {locations.map((location) => (
         <div key={uuidv4()}>
           <p>{location.id}</p>
           <p>{location.name}</p>
           <p>{location.point}</p>
         </div>
-      ))} */}
+      ))}
     </div>
   );
 }
